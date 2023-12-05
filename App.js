ad953410c3b563282bee959051f29f771413f3b2
P7qMXFXqzy <139146483+P7qMXFXqzy@users.noreply.github.com>
2023-12-05 17:20:13 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,31 @@
+import './App.css';
+import React from 'react'
+import {Helmet} from 'react-helmet';
+
+
+const textoClicavel = () => {
+    //definição da url que será aberta
+    const targetUrl = 'https://www.youtube.com/';
+
+    //Comando para abrir a página em uma nova guia
+    window.open(targetUrl, '_blank');
+}
+
+function App() {
+  return (
+    <div>
+      <>
+        {/*definição da cor de fundo*/}
+        <Helmet>
+          <style>{'body { background-color: red; }'}</style>
+        </Helmet>
+        {/*texto clicável que redirecionará para o youtube*/}
+        <a id="abrirTexto" href="/" onClick={textoClicavel}>
+            abrir Youtube
+        </a>
+      </>
+    </div>
+  );
+}
+
+export default App;
